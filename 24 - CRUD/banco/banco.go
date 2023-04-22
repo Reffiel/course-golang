e8b91a958b55383009f19b50a8ccd1c3d9b307ea
Rodrigo Reffiel <rodrigo.reffiel@gmail.com>
2023-04-23 00:43:48 +0100

    Atualização projeto de consulta

--- 24 - CRUD/banco/banco.go
+++ 24 - CRUD/banco/banco.go
@@ -8,8 +8,11 @@ import (
 
 /*Como não estamos dentro do arquivo main não se pode fechar com fatal temos de retornar algo, neste caso retornaremos ou o ponteiro de conexão ou o erro, isso porque são mutuamente exclusivos
 Exemplo de interface para conectar uma vez, na inicialização faz-se o contrato para conexão. Com a instância já se consegue utilizar todos handle
+para fazer downliad di driver my sql comando > go get github.com/go-sql-driver/mysql
 */
 func Conectar() (*sql.DB, error) {
+	/*String para conexão para mysql > usuario:senha@/nomeDoBanco?charset=utf8&parseTime=True&loc=Local
+	| postgres é diferente*/
 	stringConexao := "golang:golang@/devbook?charset=utf8&parseTimeTrue&loc=Local"
 
 	db, erro := sql.Open("mysql", stringConexao)
